018045de0c9813e249d9d981f0fcce4eb8ce0d4d
DANGVIETTOAN <1951052204toan@ou.edu.vn>
2022-11-12 21:37:47 +0700

    first commit

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model
 {
     use HasFactory;
+    protected $table = 'customer';
+    protected $timestamps = true;
+    protected $filltable = [
+        'id',
+        'customer_name',
+        'day_of_birth',
+        'gender',
+        'phone_number',
+        'id_card',
+        'address',
+        'owner',
+        'apartment_id',
+    ];
 }
